04d3013eec19bc9dde720d2440a791bc4b6f6355
Joe <73498493+jserra99@users.noreply.github.com>
2021-01-26 12:21:42 -0800

    Update main.py

--- Semester_Final/main.py
+++ Semester_Final/main.py
@@ -3,17 +3,20 @@ This will be a very hard project.
 '''
 
 #Importing necessary modules
-import pygame as pg, time, player
+import pygame as pg, time, player, pygame.freetype
 
 clock = 0
 paused = False
 available_songs = ('Megalovania')
-chosen_song = input(f"Which song would you like? : {available_songs} : ").lower()
+chosen_song = input(f"Which song would you like?, {available_songs} : ").lower()
 size = width, height = 600, 600
 pg.init()
 screen = pg.display.set_mode(size)
 screen.fill('black') #Changes the background to black, this can be changed later to an image.
-pg.display.set_caption('DDR 2021')
+display = pg.display
+display.set_caption('DDR 2021')
+display_font = pg.freetype.SysFont("Comic Sans MS", 30)
+display_font.render_to(screen, (0,0), "Press Space to Start!", fgcolor='white')
 
 
 while True:
@@ -26,4 +29,7 @@ while True:
             '''if key == pg.K_ESCAPE:
                 player.pause(paused)'''
             if key == pg.K_SPACE:
-                player.start(chosen_song)
+                display_font.render_to(screen, (0,0), "Press Space to Start!", fgcolor='black', bgcolor='black')
+                player.start(chosen_song, screen, display)
+
+    pg.display.update()
